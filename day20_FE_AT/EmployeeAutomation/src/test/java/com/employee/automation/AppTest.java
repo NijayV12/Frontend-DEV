4d6ec76089f0938f1cd4ffe8657b36a26f42b685
nijay <nijay@example.com>
2026-07-17 09:32:25 +0530

    chore: track EmployeeAutomation folder as explicitly requested by user

---
+++ day20_FE_AT/EmployeeAutomation/src/test/java/com/employee/automation/AppTest.java
@@ -0,0 +1,14 @@
+package com.employee.automation;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
